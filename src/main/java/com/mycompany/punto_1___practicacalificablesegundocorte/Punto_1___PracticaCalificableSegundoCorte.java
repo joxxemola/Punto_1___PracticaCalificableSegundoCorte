7e9529fb348d79ee64f28a7fed25f17ce5630eb8
Jose D Molano P <jdmolanoperdomo@gmail.com>
2026-04-08 21:14:12 -0500

    Commit

---
+++ src/main/java/com/mycompany/punto_1___practicacalificablesegundocorte/Punto_1___PracticaCalificableSegundoCorte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.punto_1___practicacalificablesegundocorte;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Punto_1___PracticaCalificableSegundoCorte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
